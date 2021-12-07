0834dcdb39f1c6418d8056817e924755927034f1
Adi Arifin <adiarifin15@outlook.co.id>
2021-12-07 17:11:27 +0700

    Ini adalah initial commit

---
+++ Kel10Project/Kel10Project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Kel10Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           
+        }
+    }
+}
